9b8e38c84fa73c5b208aa665c349bc1d8df837de
HHNebula <sungsoomr@icloud.com>
2022-11-15 14:06:53 +0900

    Update

--- docs/vanilla_js/input.js
+++
@@ -1,8 +0,0 @@
-let fs = require("fs");
-
-let readFileSync = fs.readFileSync("/dev/stdin");
-let toString = readFileSync.toString();
-let trim = toString.trim();
-let split = trim.split();
-
-console.log();
