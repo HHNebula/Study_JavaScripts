17050ac0e89fd6360ba6d0a6de1e8d3fa6d121ca
HHNebula <sungsoomr@icloud.com>
2022-11-15 14:26:16 +0900

    Update

---
+++ docs/vanilla_js/booleans.js
@@ -0,0 +1,18 @@
+console.log(20 > 5);
+console.log(20 < 5);
+console.log(20 == 5);
+console.log(20 === 5);
+console.log(20 != 5);
+console.log(20 !== 5);
+console.log(true && false);
+console.log(true || false);
+console.log(true && !false);
+console.log(!true || false);
+
+let intType = 5;
+let stringType = "5";
+
+console.log(intType == stringType); // 값만 비교
+console.log(intType === stringType); // 값과 타입 비교
+console.log(intType != stringType); // 값이 다른지
+console.log(intType !== stringType); // 값 또는 타입이 다른지
