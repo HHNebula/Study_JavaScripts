ea632a7c106d15715500a42c35eee1882c3fcee7
HHNebula <sungsoomr@icloud.com>
2022-11-16 15:17:52 +0900

    Update

--- docs/vanilla_js/functions_sums.js
+++ docs/vanilla_js/functions_sums.js
@@ -5,4 +5,9 @@ function totalSum(numbers) {
     for (let num of numbers) {
         answer += num;
     }
+    return answer;
 }
+
+let numbers = [ 1, 2, 3, 4, 5, 6, 7 ];
+let answer = totalSum(numbers);
+console.log(answer);
