f81e02268f9b9ebcc1d9ef1b647cb8d66a4aca91
HHNebula <sungsoomr@icloud.com>
2022-11-28 13:42:38 +0900

    Update

--- docs/browser_js/js/event_targets.js
+++ docs/browser_js/js/event_targets.js
@@ -1,7 +1,26 @@
 let querySingle = document.querySelector("#single");
 querySingle.addEventListener("click", singleEvent);
 querySingle.addEventListener("double-click", singleEvent);
-function singleEvent (event) {
+function singleEvent(event) {
     console.log(event);
     querySingle.innerHTML = "Take One Event!";
 }
+
+let queryKeydwon = document.querySelector("#keydown");
+queryKeydwon.addEventListener("keydown", event => {
+    if (event.code == "Enter") {
+        keydownDesc(queryKeydwon.value);
+    }
+});
+
+function keydownDesc(desc) {
+    let queryKeydwonDesc = document.querySelector("#keydownDesc");
+    queryKeydwonDesc.innerHTML = desc;
+}
+
+let queryload = document.querySelector("#keydownDesc");
+queryload.addEventListener("load", queryLoads);
+
+function queryLoads() {
+    queryload.innerHTML = "Done On Load";
+};
